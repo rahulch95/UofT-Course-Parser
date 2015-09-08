9a1061f5d60ee86dc81e70a7d31620a0ae8cc6cb
Rahul Chaudhary <rahul.chaudhary@mail.utoronto.ca>
2015-09-08 20:31:55 +0400

    Added code to parse coursefinder for each course

--- app/parser.py
+++ app/parser.py
@@ -3,18 +3,25 @@ import requests
 import re
 import os
 
-URL = 'http://www.artsandscience.utoronto.ca/ofr/timetable/winter/'
-
+TIMETABLE_URL = 'http://www.artsandscience.utoronto.ca/ofr/timetable/winter/'
+HTML_DATA_DIR = os.path.dirname(__file__) + '/html_data/'
+COURSE_FINDER_URL = 'http://coursefinder.utoronto.ca/course-search/search/courseInquiry?methodToCall=start&' \
+                    'viewId=CourseDetails-InquiryView&courseId='
+COURSE_FINDER_MAPPING = {
+    'Y': '20159',
+    'F': '20159',
+    'S': '20161'
+}
 
 def store_html_page(name, body):
-    f = open(os.path.dirname(__file__) + '/html_data/' + name, 'w')
+    f = open(HTML_DATA_DIR + name, 'w')
     f.write(body.encode('utf-8'))
     f.close()
 
 
 def store_html_data():
-    req = requests.get(URL)
-    f = open(os.path.dirname(__file__) + '/html_data/links.txt', 'w')
+    req = requests.get(TIMETABLE_URL)
+    f = open(HTML_DATA_DIR + 'links.txt', 'w')
     if req.status_code != 200:
         return False
 
@@ -28,11 +35,39 @@ def store_html_data():
         if not matched:
             continue
         f.write(html_link + '\n')
-        html_data = requests.get(URL + html_link)
+        html_data = requests.get(TIMETABLE_URL + html_link)
         if html_data.status_code == 200:
             store_html_page(html_link, html_data.text)
     f.close()
+    return True
 
 
+def get_course_finder_data(course_code, course_term):
+    url = COURSE_FINDER_URL + course_code + str(COURSE_FINDER_MAPPING[course_term])
+    body = requests.get(url)
+    if body.status_code != 200:
+        return False
+
+    html_body = BeautifulSoup(body.text, 'html.parse')
+    table = html_body.find('table')
+    tableData = [[col.get_text().encode('utf-8').replace('\n', '').replace('\r', '').strip() for col in row.findAll("td")] for row in table.findAll("tr")]
+    # store table data in proper format into course finder
 
 
+def parse_and_store_courses():
+    files = os.listdir(HTML_DATA_DIR)
+    for file in files:
+        if file == 'links.txt':
+            continue
+        f = open(HTML_DATA_DIR + file, 'r')
+        body = BeautifulSoup(f.read(), 'html.parser')
+        f.close()
+        table = body.find('table')
+        tableData = [[col.get_text().encode('utf-8') for col in row.findAll("td")] for row in table.findAll("tr")]
+        regex = re.compile('\w{3}\d{3}\w\d')
+        for row in tableData:
+            if len(row) and regex.match(row[0]):
+                course_code = row[0] + row[1]
+                course_term = row[1]
+                course_name = row[2]
+                get_course_finder_data(course_code, course_term)
